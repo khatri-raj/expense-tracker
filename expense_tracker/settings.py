d6205cb0b69149bbaa83d160aa41b23365a21d49
khatri-raj <rajkhatri8060@gmail.com>
2025-06-24 16:00:43 +0530

    Deploy Frontend

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -98,6 +98,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://192.168.1.3:3000",
+    "https://expensetracker-frontend-08uj.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
