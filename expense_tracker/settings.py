2dc77ab7655e0a501f652104dabbada453d5bf23
khatri-raj <rajkhatri8060@gmail.com>
2025-06-25 12:06:15 +0530

    backedn deployment

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key')
 
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['expense-tracker-backend-u02t.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
